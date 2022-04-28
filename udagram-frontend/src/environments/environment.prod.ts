428323f3d4d675e31e38fe8427987fc5e7a06802
Tarek Mohadel <tarekmohadel21@gmail.com>
2022-04-28 19:22:12 +0200

    second commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://thirdproject-env.eba-qfmpe834.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
